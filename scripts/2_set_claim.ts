6be43cd4676af3dd8ed93fa13e1c4e600fffa478
Eric Polerecky <eric@polerecky.com>
2023-01-10 11:16:36 -0500

    chore(test): add test for update claim logic

--- scripts/2_set_claim.ts
+++ scripts/2_set_claim.ts
@@ -16,7 +16,7 @@ async function main() {
   const contract = await ethers.getContractAt("AkromaClaimsRegistry", "0xB557A2968776123eb2DC402eb5D5E123c2d6c7E0") as AkromaClaimsRegistry;
   const name = await contract.name();
   const claim = await contract.setClaim(
-    '0x17218eDBBa1cEc8623D7c82ab995B0B759aB9FF6',
+    '0xB01df81Aec5D798e2216d5f5Da91aE2B7f9b6eDB',
     key,
     "eyJ1c2VybmFtZSI6ImFzZGEiLCJpbnZpdGVkIjpbXSwib3duZWQiOltdfQ==",
   { value: '10000000000000000'});
